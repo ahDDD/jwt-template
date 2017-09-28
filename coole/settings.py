df75fe9a65ea8ea2645a33787b473b664cebcb9e
d <ddsweet520@qq.com>
2017-09-28 21:02:39 +0800

    fix

--- coole/settings.py
+++ coole/settings.py
@@ -150,7 +150,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/frontend/dist/'.replace('//', '/')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # build static out
 STATICFILES_DIRS = [
