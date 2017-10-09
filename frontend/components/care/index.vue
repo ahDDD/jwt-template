53fc12a5b5d7e9ed34e067a004067fb980992361
d <ddsweet520@qq.com>
2017-10-09 22:11:47 +0800

    fix

--- frontend/components/care/index.vue
+++ frontend/components/care/index.vue
@@ -2,6 +2,8 @@
 mu-content-block.care
   h1 身体小问题, 快速找医生
   mu-raised-button.login-button(label="找医生" fullWidth primary @click="$router.push({ name: 'care-classify' })")
+  footer
+    img(src="~static/main_logo.png")
 </template>
 
 <script>
@@ -22,6 +24,13 @@ export default {
 
 <style lang="stylus">
 .care
+  footer
+    height 74vh
+    display flex
+    justify-content center
+    align-items flex-end
+    img
+      height 2em
   h1
     color #78909c
     text-align center
